f375f3d3fc129510df619687d155a94d26a6800d
Ed D'Azevedo <dazevedoef@ornl.gov>
2021-03-12 14:40:33 -0600

    silence warning

--- debug_cray.sh
+++ debug_cray.sh
@@ -1,13 +1,13 @@
 echo " == serial == "
-ftn -h noacc -h noomp -o test_kron_mod_serial \
+ftn -M7256  -h noacc -h noomp -o test_kron_mod_serial \
 	prec_mod.F90 kron_mod.F90 test_kron_mod.F90
 
 echo "== OpenACC == "
-ftn -h acc -h noomp -target-accel=amd_gfx906 \
+ftn -M7256  -h acc -h noomp  \
 	-o test_kron_mod_acc \
 	prec_mod.F90 kron_mod.F90 test_kron_mod.F90
 
 echo "== OpenMP == "
-ftn -eZ -hlist=a -h noacc -h omp -DOMP_TARGET -target-accel=amd_gfx906 \
+ftn -M7256  -eZ -hlist=a -h noacc -h omp -DOMP_TARGET  \
 	-o test_kron_mod_omp \
 	prec_mod.F90 kron_mod.F90 test_kron_mod.F90
