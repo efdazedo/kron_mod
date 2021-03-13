ca8432c471328fe8710d7d68c647b34e302192dc
D'azevedo, Ed <dazevedoef@ornl.gov>
2021-03-12 21:55:40 -0500

    update

--- debug_acc_double.sh
+++ debug_acc_double.sh
@@ -6,7 +6,7 @@ nvfortran -DUSE_DOUBLE -cpp -E -F -UOMP_TARGET -D_OPENACC kron_mod.F90 > kron_mo
 
 echo "=== with -Minline ==== "
 nvfortran -DUSE_DOUBLE  -Mstandard -Mfixed  \
-	-acc -ta=tesla -Minfo=all \
+	-acc=verystrict  -ta=tesla -Minfo=all \
 	-Minline=size:99,reshape \
         -UOMP_TARGET \
 	-c prec_mod.F90 kron_mod.f90  
@@ -14,14 +14,14 @@ nvfortran -DUSE_DOUBLE  -Mstandard -Mfixed  \
 
 echo "=== without -Minline ==== "
 nvfortran -DUSE_DOUBLE  -Mstandard -Mfixed  \
-	-acc -ta=tesla,keepgpu,keepptx  -Minfo=all \
+	-acc=verystrict  -ta=tesla,keepgpu,keepptx  -Minfo=all \
         -UOMP_TARGET \
 	-c prec_mod.F90 kron_mod.f90  
 
 
 echo "=== link -Minline ==== "
 nvfortran -DUSE_DOUBLE  -Mstandard -Mfixed  \
-	-acc -ta=tesla,keepgpu,keepptx  -Minfo=all \
+	-acc=verystrict  -ta=tesla,keepgpu,keepptx  -Minfo=all \
         -UOMP_TARGET \
 	-o test_kron_mod_acc \
 	 prec_mod.o kron_mod.o test_kron_mod.F90  
