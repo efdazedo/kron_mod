bb7e8ce671de3dda620228f773c7bb48f849d1f3
D'azevedo, Ed <dazevedoef@ornl.gov>
2021-03-12 20:21:33 -0500

    default complex

--- debug_acc.sh
+++ debug_acc.sh
@@ -1,25 +1,27 @@
 
+touch test_kron_mod_acc
+rm test_kron_mod_acc
 
-pgf90 -cpp -E -F -UOMP_TARGET -D_OPENACC kron_mod.F90 > kron_mod.f90
+nvfortran -UUSE_DOUBLE -cpp -E -F -UOMP_TARGET -D_OPENACC kron_mod.F90 > kron_mod.f90
 
 echo "=== with -Minline ==== "
-nvfortran -Mstandard -Mfixed  \
-	-acc -ta=tesla,cc70,nollvm  -Minfo=all \
+nvfortran -UUSE_DOUBLE  -Mstandard -Mfixed  \
+	-acc -ta=tesla -Minfo=all \
 	-Minline=name:zgemm,reshape \
         -UOMP_TARGET \
 	-c prec_mod.F90 kron_mod.f90  
 
 
 echo "=== without -Minline ==== "
-nvfortran -Mstandard -Mfixed  \
-	-acc -ta=tesla,cc70,keepgpu,keepptx  -Minfo=all \
+nvfortran -UUSE_DOUBLE  -Mstandard -Mfixed  \
+	-acc -ta=tesla,keepgpu,keepptx  -Minfo=all \
         -UOMP_TARGET \
 	-c prec_mod.F90 kron_mod.f90  
 
 
 echo "=== link -Minline ==== "
-nvfortran -Mstandard -Mfixed  \
-	-acc -ta=tesla,cc70,keepgpu,keepptx  -Minfo=all \
+nvfortran -UUSE_DOUBLE  -Mstandard -Mfixed  \
+	-acc -ta=tesla,keepgpu,keepptx  -Minfo=all \
         -UOMP_TARGET \
 	-o test_kron_mod_acc \
 	 prec_mod.o kron_mod.o test_kron_mod.F90  
