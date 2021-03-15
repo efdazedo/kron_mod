1e63944ac3dcfe59a0d209df790b688d64fdfbf2
D'azevedo, Ed <dazevedoef@ornl.gov>
2021-03-15 11:04:45 -0400

    update for XL OpenMP target

--- build_xl.sh
+++ build_xl.sh
@@ -1,4 +1,7 @@
 module load xl cuda essl
+touch prec_mod.mod 
+rm *.mod *.o
+
 xlf90_r  -d -qfixed=72 -qlist -qattr=full -qsmp=omp -qoffload \
   -WF,-qfpp=linecont \
   -DOMP_TARGET -U_OPENACC -c \
@@ -10,10 +13,13 @@ xlf90_r  -d -qfixed=72 -qlist -qattr=full -qsmp=omp -qoffload \
   kron_mod.F90 
 
 
+touch test_kron_mod_omp
+rm test_kron_mod_omp
+
 xlf90_r  -d -qfixed=72 -qlist -qattr=full -qsmp=omp -qoffload \
   -WF,-qfpp=linecont \
   -DOMP_TARGET -U_OPENACC \
   prec_mod.o kron_mod.o test_kron_mod.F90 \
   -o test_kron_mod_omp \
-  -l$OLCF_ESSL_ROOT/lib64 -lessl
+  -L $OLCF_ESSL_ROOT/lib64 -lessl
 
