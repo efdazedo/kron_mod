36d9445e8fe0856382973904c81e2162bb2eb0d5
D'azevedo, Ed <dazevedoef@ornl.gov>
2021-03-26 21:41:41 -0400

    add option to use more work space

--- kronmult2.m
+++ kronmult2.m
@@ -6,12 +6,46 @@ function Y = kronmult2(m1,n1,A1,  m2,n2,A2, nvec, X )
 % Y = A2 * X * transpose(A1)
 % step 1: W = X * transpose(A1)
 % step 2: Y = A2 * W
+% -----------
+X = reshape( X, [n2*n1, nvec] );
 
-X = reshape( X, n2*n1, nvec );
-for i=1:nvec,
+use_W = 1;
+if (use_W),
+  % ---------------------------------
+  % use more temporary work space in W
+  % larger new_nvec call to kronmult1
+  % ---------------------------------
+  W = zeros( n2*m1, nvec );
+  for i=1:nvec,
 	Xi = reshape(X(:,i), [n2,n1]);
 	Wi = Xi(1:n2, 1:n1) * transpose( A1(1:m1,1:n1) );
-	Y(1:(m2*m1),i) = reshape( A2(1:m2,1:n2) * Wi(1:n2,1:m1),   m2*m1, 1);
-end 
+	W(1:(n2*m1), i) = reshape( Wi, [n2*m1,1]);
+  end;
+
+  % ------------------------------
+  % larger single call to kronmult
+  % ------------------------------
+  new_nvec = nvec * m1;
+  W = reshape( W, [n2, new_nvec]);
+  Y = zeros( [m2, new_nvec]);
+  Y(1:m2,1:new_nvec) = kronmult1( m2,n2,A2, new_nvec, W );
+  Y = reshape( Y, [m2*m1, nvec] );
+else
+  % -----------------------------------
+  % use less temporary work space in Wi
+  % more calls matrix multiply
+  % -----------------------------------
+  Y = zeros( m2*m1,  nvec );
+  for i=1:nvec,
+	Xi = reshape(X(:,i), [n2,n1]);
+	Wi = Xi(1:n2, 1:n1) * transpose( A1(1:m1,1:n1) );
+	new_nvec = m1;
+	Yi = kronmult1( m2,n2,A2,  new_nvec, Wi);
+	Y(1:(m2*m1),i) = reshape( Yi, [m2*m1,1]);
+  end 
+end;
+
+
+end
 
 
--- kronmult3.m
+++ kronmult3.m
@@ -4,15 +4,43 @@ function Y = kronmult3(m1,n1,A1,  m2,n2,A2, m3,n3,A3, nvec, X )
 % Y = kron(A2,A3)*X * transpose(A1)
 % step 1: W = X * transpose(A1)
 % step 2: Y = kron(A2,A3)*W
+% ---------------
 
 X = reshape( X, [n3*n2*n1, nvec]);
-for i=1:nvec,
+
+use_W = 1;
+if (use_W),
+  % ----------------------------------------
+  % use more temporary work space in W
+  % but larger new_nvec in call to kronmult2
+  % ----------------------------------------
+  W = zeros( [n3*n2*m1, nvec ]);
+  for i=1:nvec,
+	Xi = reshape( X(1:(n3*n2*n1),i), [n3*n2, n1]);
+	Wi = Xi(1:(n3*n2),1:n1)  * transpose( A1(1:m1,1:n1) );
+	W(1:(n3*n2*m1),i) = reshape( Wi, [n3*n2*m1,1]);
+  end;
+
+  % -------------------------------
+  % larger single call to kronmult2
+  % -------------------------------
+  new_nvec = nvec * m1;
+  Y = kronmult2( m2,n2,A2,   m3,n3,A3, new_nvec, W);
+  Y = reshape( Y, [m3*m2*m1, nvec ] );
+else
+  % -----------------------------------
+  % use less temporary work space in Wi
+  % but more calls to kronmult2
+  % -----------------------------------
+  Y = zeros( (m3*m2*m1), nvec );
+  for i=1:nvec,
 	Xi = reshape( X(1:(n3*n2*n1),i), [n3*n2, n1]);
 	Wi = Xi(1:(n3*n2),1:n1)  * transpose( A1(1:m1,1:n1) );
 	Wi = reshape( Wi, [n3*n2, m1]);
 	new_nvec = m1;
-	Y(1:(m3*m2*m1),i) = ...
-	  reshape(kronmult2( m2,n2,A2,m3,n3,A3,new_nvec,Wi),[m3*m2*m1,1]);
-end
+	Yi = kronmult2( m2,n2,A2,m3,n3,A3,new_nvec,Wi);
+	Y(1:(m3*m2*m1),i) = reshape(Yi, [m3*m2*m1,1]);
+  end
+end;
 
 end
