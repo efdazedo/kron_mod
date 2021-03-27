a463c862cd6c28e8a597e124d407f827d6651934
D'azevedo, Ed <dazevedoef@ornl.gov>
2021-03-26 20:53:17 -0400

    fix typo

--- kronmult1.m
+++ kronmult1.m
@@ -2,6 +2,6 @@ function Y = kronmult1( m1,n1, A1, nvec, X )
 % Y = kronmult1( m1,n1, A1, nvec, X )
 % implemented as single matrix multiply
 %
-Y(1:m, 1:nvec)  = A1(1:m1,1:n1) * X(1:n1, 1:nvec);
+Y(1:m1, 1:nvec)  = A1(1:m1,1:n1) * X(1:n1, 1:nvec);
 end
 
